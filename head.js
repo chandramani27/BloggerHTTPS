4286d5f50a7c96c0bd35d11c673a2f097191ddc6
Chandramani Kumar <chandramanikumar2010@gmail.com>
2017-05-21 13:25:53 +0530

    Create head.js

---
+++ head.js
@@ -0,0 +1,6 @@
+<script>
+$(document).ready(function() 
+{           $("a").each(function() 
+{             var i = $(this).attr("href");            
+ var n = i.replace(http://www.yourdomain.com, "https://www.yourdomain.com");
+ </script>
